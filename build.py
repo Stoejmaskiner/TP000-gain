098329a8f7027c8465a2c0f6167126c9236f79f4
Lorenzo Panieri <panierilorenzo@gmail.com>
2023-05-28 15:20:53 +0200

    adding missing dev dependencies

--- build.py
+++ build.py
@@ -1,8 +1,9 @@
-
 APT_DEV_DEPS = [
     "libgl1-mesa-dev",
     "libxcursor-dev",
-    "libxkbcommon-x11-dev"
+    "libxkbcommon-x11-dev",
+    "libxcb1-dev",
+    "libx11-xcb-dev",
+    "libxcb-icccm4-dev",
+    "libxcb-dri2-0-dev",
 ]
-
-
